5e8f9d653b99a275e2d59faa07c3f0126f190a90
Clayton Lengel-Zigich <clayton@claytonlz.com>
2013-12-19 13:33:36 -0700

    Layout and pages boilerplate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Playopenface::Application.routes.draw do
+  get "pages/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
